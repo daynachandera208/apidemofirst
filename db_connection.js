d99223e94ef67d4257c2392cdde376957f604a9f
iMac <>
2021-01-12 16:21:04 +0530

    sdf

--- db_connection.js
+++ db_connection.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "infozium.in",
-  password: "pbqtST9nx7isrjD",
-  database : "gaming_infozium"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "infozium.in",
+//   password: "pbqtST9nx7isrjD",
+//   database : "gaming_infozium"
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
 module.exports = con;
--- dbconfig.js
+++ dbconfig.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
-const config = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345678",
-  database : "playmax"
-})
+// const config = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "12345678",
+//   database : "playmax"
+// })
 
-config.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// config.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
-module.exports = config;
+// module.exports = config;
